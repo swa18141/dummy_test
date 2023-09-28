0ff8d0940f52521069e8046ee2c4fbacc49ea564
swa18141 <127288802+swa18141@users.noreply.github.com>
2023-09-28 16:41:45 +0530

    Update api-trigger.ps1

--- .github/workflows/api-trigger.ps1
+++ .github/workflows/api-trigger.ps1
@@ -1,18 +1,38 @@
-# Specify the API URL
+# Define your API endpoint URL
 $apiUrl = "https://mocktarget.apigee.net/json"
 
-try {
-    # Make the API request using Invoke-RestMethod
-    $response = Invoke-RestMethod -Uri $apiUrl -Method Get -ContentType "application/json" -ErrorAction Stop -TimeoutSec 60
-
-    # Handle the API response here
-    Write-Host "HTTP Status Code: $($response.StatusCode)"
-    Write-Host "Response Content: $($response.Content)"
-
-    # Save the response to a file
-    $response.Content | Out-File "test.json" -Encoding UTF8
-} catch {
-    # Handle any errors that occur during the API request
-    Write-Host "Error occurred: $_"
+# Define the request headers (if needed)
+$headers = @{
+    "Content-Type" = "application/json"
+    # Add any other headers here if required
 }
+
+# Send a POST request to the API endpoint
+Invoke-RestMethod -Uri $apiUrl -Method:Get -ContentType "application/json" -ErrorAction:Stop -TimeoutSec 60 
+
+$response = Invoke-RestMethod -Uri $apiUrl -Method:Get -ContentType "application/json" -ErrorAction:Stop -TimeoutSec 60
+
+# Specify the path to the existing file in your repository
+$filePath = "path/to/your/file.txt"
+
+# Sample data to add to the file
+$sampleData = "This is some sample data to add to the file."
+
+# Read the existing content of the file
+$fileContent = Get-Content -Path $filePath -Raw
+
+# Append the sample data to the file content
+$newContent = $fileContent + "`n" + $sampleData
+
+# Write the updated content back to the file
+$newContent | Set-Content -Path $filePath
+
+# Commit the changes to the repository
+git add $filePath
+git commit -m "Add sample data to file.txt"
+git push
+
+# Print the response (optional)
+Write-Host "API Response:"
+Write-Host $response
 
