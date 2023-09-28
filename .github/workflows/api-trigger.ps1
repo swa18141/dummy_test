94c8719a3cb65e1e13584fff11fa61c348636abc
swa18141 <127288802+swa18141@users.noreply.github.com>
2023-09-28 11:58:35 +0530

    Create api-trigger.ps1

---
+++ .github/workflows/api-trigger.ps1
@@ -0,0 +1,18 @@
+
+# Define your API endpoint URL
+$apiUrl = "https://mocktarget.apigee.net/json"
+
+# Define the request headers (if needed)
+$headers = @{
+    "Content-Type" = "application/json"
+    # Add any other headers here if required
+}
+
+# Send a POST request to the API endpoint
+Invoke-RestMethod -Uri $apiUrl -Method:Get -ContentType "application/json" -ErrorAction:Stop -TimeoutSec 60 -OutFile "test.json"
+
+$response = Invoke-RestMethod -Uri $apiUrl -Method:Get -ContentType "application/json" -ErrorAction:Stop -TimeoutSec 60
+
+# Print the response (optional)
+Write-Host "API Response:"
+Write-Host $response
